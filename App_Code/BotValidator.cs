4b1d71065346bc4dc798c2e91aaf94fc71b6dd47
INDQ_Studio <72544563+IndqStudio@users.noreply.github.com>
2022-04-10 01:07:10 +0530

    Add files via upload

---
+++ App_Code/BotValidator.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Sinuvera
+{
+    public class InvisibleBotValidator
+    {
+
+        #region "Constants and Statics"
+
+        // Statics
+        private static SortedList<DateTime, string> _pastAddresses = new SortedList<DateTime, string>();
+
+        private static SortedList<string, DateTime> _requestKeys = new SortedList<string, DateTime>();
+        //Contants
+        private const int ResponseMinimumDelaySeconds = 4;
+        private const int CutoffWindowSeconds = 14400;
+        private const int CutoffMaximumInstances = 6;
+        private const string SignatureHeaderName = "X-DataSign";
+        private const string KeyFieldName = "Bot_KeyField";
+        #endregion
+
+        #region "Methods"
+
+        public static string Load()
+        {
+            DateTime utcNow = DateTime.UtcNow;
+            string requestKey = Guid.NewGuid().ToString("N");
+            lock (_requestKeys)
+            {
+                _requestKeys.Add(requestKey, utcNow.AddSeconds(ResponseMinimumDelaySeconds));
+            }
+            return requestKey;
+        }
+
+        public static string getHashSha256(string text)
+        {
+            StringBuilder op = new StringBuilder();
+            byte[] hash = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(text));
+            foreach (byte x in hash)
+            {
+                op.Append(x.ToString("x2"));
+            }
+            return op.ToString();
+        }
+
+        public static string EvaluateIsValid()
+        {
+            try
+            {
+                DateTime utcNow = DateTime.UtcNow;
+                // Report invalid if response arrives too soon
+                //
+                lock (_requestKeys)
+                {
+                    // Get the request Key from the POSTed value
+                    string requestKey = HttpContext.Current.Request.Form[KeyFieldName];
+
+                    //Remove expired keys older than 6 hours
+                    List<string> ExpiredEntries = _requestKeys.Where(x => x.Value < utcNow.AddHours(-6)).Select(y => y.Key).ToList();
+                    foreach (string ky in ExpiredEntries)
+                    {
+                        _requestKeys.Remove(ky);
+                    }
+
+                    // Fail if request key is not found or if the response came too soon
+                    if (string.IsNullOrEmpty(requestKey) || (!_requestKeys.ContainsKey(requestKey)))
+                    {
+                        return "Request Key not found";
+                    }
+                    else if (utcNow < _requestKeys[requestKey])
+                    {
+                        return "Response came too soon";
+                    }
+                }
+
+                // Report invalid if response is wrong
+                //
+                string body = "";
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(HttpContext.Current.Request.InputStream))
+                {
+                    try
+                    {
+                        reader.BaseStream.Position = 0;
+                        body = reader.ReadToEnd();
+                    }
+                    finally
+                    {
+                        reader.BaseStream.Position = 0;
+                    }
+                }
+                string requiredResponse = getHashSha256(body);
+                if (requiredResponse != HttpContext.Current.Request.Headers[SignatureHeaderName])
+                {
+                    return "Signature Mismatch";
+                }
+
+                // Report invalid if too many responses from same IP address
+                lock (_pastAddresses)
+                {
+                    // Add user address to address cache, taking care not to duplicate keys
+                    string userAddress = HttpContext.Current.Request.UserHostAddress;
+                    DateTime utcAdd = utcNow.AddSeconds(CutoffWindowSeconds);
+                    while (_pastAddresses.ContainsKey(utcAdd))
+                    {
+                        utcAdd = utcAdd.AddTicks(1);
+                    }
+                    _pastAddresses.Add(utcAdd, userAddress);
+
+                    //Remove expired addresses
+                    List<DateTime> ExpiredEntries = _pastAddresses.Keys.Where(x => x < utcNow).ToList();
+                    foreach (DateTime dt in ExpiredEntries)
+                    {
+                        _pastAddresses.Remove(dt);
+                    }
+
+                    // Determine number of instances of user address in cache
+                    int instances = _pastAddresses.Values.Where(x => x == userAddress).Count();
+                    // Fail if too many
+                    if (CutoffMaximumInstances < instances)
+                    {
+                        return "IP Address is too active";
+                    }
+                }
+
+                // All checks OK, report valid
+                //
+                return "";
+            }
+            catch (NullReferenceException)
+            {
+                return "Bad Session";
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ App_Code/Email.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+
+namespace FLSModel
+{
+
+    public class Email
+    {
+        public Email()
+        {
+            EmailAddress = string.Empty;
+            Subject = string.Empty;
+            Body = string.Empty;
+            smtp = null;
+        }
+
+        #region "Properties"
+        public SmtpClient smtp { get; set; }
+        public string EmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public Dictionary<int, string> Attachments{ get; set; }
+
+        #endregion
+
+        #region "Helper For Send Mail"
+        private void SendMail(MailMessage ObjMail)
+        {
+            if (smtp == null)
+            {
+                smtp = new SmtpClient();
+            }
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Send(ObjMail);
+            ObjMail.Dispose();
+            ObjMail = null;
+            smtp.Dispose();
+            smtp = null;
+        }
+
+        public bool SendHtmlEmail(string Bcc = "", string Cc = "", string FromAddress = "")
+        {
+            try
+            {
+                MailMessage ObjMail = new MailMessage();
+                EmailAddress = EmailAddress.Replace(";", ",").TrimEnd(",".ToCharArray()).TrimEnd(";".ToCharArray());
+                ObjMail.To.Add(EmailAddress);
+                if ((!string.IsNullOrEmpty(Bcc)))
+                {
+                    ObjMail.Bcc.Add(Bcc);
+                }
+                if ((!string.IsNullOrEmpty(Cc)))
+                {
+                    ObjMail.CC.Add(Cc);
+                }
+                if ((!string.IsNullOrEmpty(FromAddress)))
+                {
+                    ObjMail.From = new MailAddress(FromAddress);
+                }
+
+                ObjMail.Subject = this.Subject;
+                ObjMail.Body = this.Body;
+                ObjMail.IsBodyHtml = true;
+                if (Attachments != null && Attachments.Values.Count > 0)
+                {
+                    foreach (String attachpath in Attachments.Values)
+                    {
+                        try
+                        {
+                            Attachment mailAttachment = new Attachment(attachpath.ToString());
+                            ObjMail.Attachments.Add(mailAttachment);
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+                SendMail(ObjMail);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        #endregion
+    }
+
+}
